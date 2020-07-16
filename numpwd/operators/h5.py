8b3e02f2521a8e8bd9d740e6aace70e5d8e5038f
Christopher Körber <software@ckoerber.com>
2020-07-16 15:42:31 +0200

    Started to prepare h5 writer

--- numpwd/operators/h5.py
+++ numpwd/operators/h5.py
@@ -5,8 +5,23 @@ from re import search
 from h5py import File
 from pandas import DataFrame
 
-from numpwd.utils.h5io import get_dsets
+from sympy import AtomicExpr, Expr
+from datetime import datetime
+
+from numpwd.utils.h5io import get_dsets, write_data, H5ValuePrep
 from numpwd.densities.base import Density
+
+
+def prep_sympy(expr):
+    return {"data": str(expr)}, {"dtype": "sympy"}
+
+
+def prep_datetime(dt):
+    format = "%Y-%m-%d %H:%M:%S"
+    return {"data": dt.strftime(format)}, {"dtype": "datetime", "format": format}
+
+
+H5_VALUE_PREP = H5ValuePrep({(AtomicExpr, Expr): prep_sympy, datetime: prep_datetime})
 
 
 def read_h5(filename: str) -> Density:
--- numpwd/utils/h5io.py
+++ numpwd/utils/h5io.py
@@ -1,8 +1,8 @@
 """H5py tools to simplify parsing h5files."""
-from typing import Optional, Union, Dict
+from typing import Optional, Union, Dict, Any, Tuple, Callable
 from os import path
 
-from numpy import ndarray
+from numpy import ndarray, array
 from h5py import File, Group, Dataset
 
 
@@ -42,3 +42,78 @@ def get_dsets(
             dsets.update(get_dsets(obj, parent_name=address, load_dsets=load_dsets))
 
     return dsets
+
+
+class H5ValuePrep:
+    """Wrapper class which converts arbitrary dtypes into h5 writable format.
+    """
+
+    def __init__(
+        self,
+        registry: Dict[
+            object, Callable[[object], Tuple[Dict[str, Any], Dict[str, Any]]]
+        ] = None,
+    ):
+        self.registry = registry or {}
+
+    def __call__(self, obj) -> Tuple[Dict[str, Any], Dict[str, Any]]:
+        """Try parsing data to expected shape."""
+        if isinstance(obj, ndarray) or isinstance(obj, str):
+            return {"data": obj}, {}
+        else:
+            try:
+                if array(obj).dtype != object:
+                    return {"data": obj}, {}
+            except Exception:
+                pass
+
+        obj_type = str(type(obj))
+        for cls, prep_fcn in self.registry.items():
+            if isinstance(obj, cls):
+                return prep_fcn(obj)
+
+        raise TypeError(f"Don't know how to prepare data of type {obj_type}")
+
+
+def write_data(
+    data: Dict[str, Any],
+    container: Union[File, Group],
+    parent_name: Optional[str] = None,
+    h5_value_prep: Optional[H5ValuePrep] = None,
+    **kwargs,
+) -> Dict[str, Union[Dataset, ndarray]]:
+    """Access an HDF5 container and (recursivly) write dictionary keys into datasets.
+
+    Arguments:
+        container: Union[h5py.File, h5py.Group]
+            The HDF5 group or file to iteratetively search.
+
+        parent_name: Optional[str] = None
+            The name of the parent container.
+    """
+    h5_value_prep = h5_value_prep or H5ValuePrep()
+
+    for key, val in data.items():
+        if not isinstance(key, str):
+            raise TypeError(
+                f"Key {parent_name}/{key} has illeagel format."
+                " Can only write string keys."
+            )
+
+        address = path.join(parent_name, key) if parent_name else key
+
+        if isinstance(val, dict):
+            write_data(val, container, parent_name=address)
+        elif isinstance(val, list):
+            write_data(
+                {str(n): val for n, val in enumerate(val)},
+                container,
+                parent_name=address,
+            )
+        elif val is None:
+            continue
+        else:
+            options, attrs = h5_value_prep(val)
+            dset = container.create_dataset(address, **kwargs, **options)
+            for key, val in attrs.items():
+                dset.attrs[key] = val
