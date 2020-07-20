064f36dbd9e834a8873711d10a1e71d877cbb2b9
Christopher Koerber <software@ckoerber.com>
2020-07-20 09:36:01 +0200

    repaired imports

--- numpwd/operators/__init__.py
+++ numpwd/operators/__init__.py
@@ -1,3 +1,5 @@
 """Implementation of operators for folding."""
 from numpwd.operators.base import Operator  # noqa
-from numpwd.operators.h5 import read_h5  # noqa
+from numpwd.operators.h5 import read as read_h5  # noqa
+from numpwd.operators.h5 import write as write_h5  # noqa
+from numpwd.operators.expression import decompose_operator  # noqa
