916bfb47b6d4cabfaacbaa08fbbb71ae682810e8
Christopher Koerber <software@ckoerber.com>
2020-08-25 12:28:46 +0200

    Added yaml reader for op pwd

---
+++ numpwd/operators/yml.py
@@ -0,0 +1,46 @@
+"""Decompose operator from expression using yml files."""
+from logging import getLogger, basicConfig, DEBUG, INFO
+
+from yaml import safe_load
+from json import dumps
+
+from numpwd.operators.expression import decompose_operator
+
+LOGGER = getLogger("numpwd")
+
+
+def read_yml(filename: str):
+    """Read yml file and call decompose_operator."""
+    LOGGER.info("Reading `%s`", filename)
+    with open(filename, "r") as inp:
+        data = safe_load(inp)
+    LOGGER.info("Data\n:%s", dumps(data))
+    op = decompose_operator(**data)
+
+    return op
+
+
+def main():
+    """CLI for yaml read."""
+    from argparse import ArgumentParser
+
+    level_config = {"debug": DEBUG, "info": INFO}
+
+    parser = ArgumentParser(description="Run numpwd for operator over yml file.")
+    parser.add_argument("input", help="Name of the yml input file", type=str)
+    parser.add_argument(
+        "-l",
+        "--log",
+        help="Provide logging level.",
+        choices=["debug", "info"],
+        default="info",
+    )
+    args = parser.parse_args()
+    level = level_config.get(args.log)
+    basicConfig(level=level)
+
+    read_yml(args.input)
+
+
+if __name__ == "__main__":
+    main()
