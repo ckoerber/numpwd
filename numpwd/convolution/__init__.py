aa6f4aaee4238ccf60b9143a61681aece3acfb48
Christopher Koerber <software@ckoerber.com>
2020-12-23 17:16:46 +0100

    Added isospin and momentum checks to convolution

--- numpwd/convolution/__init__.py
+++ numpwd/convolution/__init__.py
@@ -7,10 +7,18 @@ from numpwd.densities import Density
 from numpwd.operators import Operator
 
 
-def convolute(dens: Density, op: Operator) -> np.ndarray:
+def convolute(dens: Density, op: Operator, tol: float = 1.0e-7) -> np.ndarray:
     """Convolutes density and operator."""
     assert len(dens.matrix.shape) == 3
-    assert len(op.matrix.shape) == 3
+    if len(op.matrix.shape) == 3:
+        matrix = op.matrix
+    if len(op.matrix.shape) == 4:
+        assert op.args[2][0] in ("k", "q", "qval")
+        q_diff = np.abs(op.args[2][1] - dens.current_info["qval"])
+        q_idx = np.argmin(q_diff)
+        assert q_diff[q_idx] < tol
+        matrix = op.matrix[:, :, :, q_idx]
+
     assert isinstance(op.isospin, pd.DataFrame)
     assert "iso" in op.isospin.columns
     np.testing.assert_allclose(dens.p, op.args[0][1])
@@ -20,13 +28,9 @@ def convolute(dens: Density, op: Operator) -> np.ndarray:
     idx1, idx2 = get_channel_overlap_indices(dens.channels, op.channels)
 
     # Compute the isospin matrix element
-    iso_fact = (
-        pd.merge(dens.channels.loc[idx1], op.isospin, how="left")["iso"]
-        .fillna(0)
-        .values
-    )
+    iso_fact = pd.merge(dens.channels.loc[idx1], op.isospin, how="inner")["iso"].values
     weight = (dens.p ** 2 * dens.wp).reshape(-1, 1)
     weight = weight.T * weight
     return (
-        (dens.matrix[idx1] * op.matrix[idx2] * weight).sum(axis=(1, 2)) * iso_fact
+        (dens.matrix[idx1] * matrix[idx2] * weight).sum(axis=(1, 2)) * iso_fact
     ).sum()
